ace3372f32f3429fa67572eee55463fe94004db1
lincoln-lm <73306575+lincoln-lm@users.noreply.github.com>
2025-08-08 16:32:09 -0600

    fix level cap increase

--- source/program/patches/level_cap.hpp
+++ source/program/patches/level_cap.hpp
@@ -5,7 +5,7 @@
 HOOK_DEFINE_INLINE(TweakLevelCap) {
     static void Callback(exl::hook::nx64::InlineCtx* ctx) {
         if (!save_file.level_cap_boost.enabled) return;
-        ctx->W[8] *= 1.5;
+        ctx->W[9] *= 1.5;
     }
 };
 
